231388e9bddcb9311efdcfbf2b55dcbf4b4bbcd6
HodRZ <hodaifa.radwan@gmail.com>
2022-09-04 22:49:03 +0300

    Fix port

--- index.js
+++ index.js
@@ -3,8 +3,8 @@
 require('dotenv').config()
 const { radio, app } = require('./modules/general/server.js')
 // const { handlePostUser } = require('./modules/person/handlePostPerson.js')
-const port =
+const port = process.env.PORT
 
-    radio(3001)
+radio(port)
 
 
